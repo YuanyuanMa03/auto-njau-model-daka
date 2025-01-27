55ecd5d5ed621f4df3ba16429aada8d844603b42
Little-King <110970384+Little-King2022@users.noreply.github.com>
2025-01-27 21:18:26 +0800

    2025-01-27版本更新
    新增自动登录功能，优化了一些细节

--- daka_web/src/App.vue
+++ daka_web/src/App.vue
@@ -2,9 +2,15 @@
 import { ref } from 'vue';
 import { Toast } from 'tdesign-mobile-vue';
 import axios from 'axios';
+import { h } from 'vue';
+import { IconFont } from 'tdesign-icons-vue-next';
+import { CloseIcon, CheckIcon } from 'tdesign-icons-vue-next';
 
+const icons = [h(CheckIcon, { size: '20px' }), h(CloseIcon, { size: '20px' })];
 
+const overlay_visible = ref(false); // 控制遮罩层显示状态
 const token = ref('');
+const auto_login = ref(false);
 const has_verified = ref(false)
 const has_tested = ref(false)
 const account_info = ref({})
@@ -12,20 +18,20 @@ const today_status = ref({})
 const daka_config = ref({})
 const errorMessage = ref(''); // 错误提示信息
 
-
-if (localStorage.getItem('token')) {
-  token.value = localStorage.getItem('token');
-  has_verified.value = true;
-}
+// 卡片折叠状态控制
+const isCollapsed1 = ref(true);
+const isCollapsed2 = ref(true);
+isCollapsed1.value = localStorage.getItem('isCollapsed1') === 'true' ? true : false;
+isCollapsed2.value = localStorage.getItem('isCollapsed2') === 'true' ? true : false;
 
 
 const verify_input = () => {
   if (token.value.length === 36) {
     has_verified.value = true;
-    errorMessage.value = ''; // 清除错误信息
+    errorMessage.value = '';
   } else {
     has_verified.value = false;
-    errorMessage.value = 'Token 长度应为 36 位，请检查后重新输入'; // 显示错误信息
+    errorMessage.value = 'Token 长度应为 36 位，请检查后重新输入';
   }
 };
 
@@ -40,6 +46,7 @@ const test_token = () => {
           // 将token保存到本地存储
           localStorage.setItem('token', token.value);
           localStorage.setItem('token_time', new Date().getTime());
+          overlay_visible.value = false;
           Toast({
             duration: 3000,
             theme: 'success',
@@ -54,8 +61,12 @@ const test_token = () => {
       .catch(error => {
         console.error(error);
         Toast('网络异常，请稍后再试');
+      })
+      .finally(() => {
+        overlay_visible.value = false;
       });
   } else {
+    overlay_visible.value = false;
     Toast('Token长度为36位 请检查后重新输入');
   }
 }
@@ -123,11 +134,37 @@ const get_daka_config = async () => {
     });
 }
 
+const toggleCard = (index) => {
+  if (index === 1) {
+    isCollapsed1.value = !isCollapsed1.value;
+    localStorage.setItem('isCollapsed1', isCollapsed1.value);
+  } else if (index === 2) {
+    isCollapsed2.value = !isCollapsed2.value;
+    localStorage.setItem('isCollapsed2', isCollapsed2.value);
+  }
+}
 
+const save_auto_login = () => {
+  localStorage.setItem('auto_login', auto_login.value);
+}
+
+if (localStorage.getItem('auto_login')) {
+  auto_login.value = localStorage.getItem('auto_login') === 'true' ? true : false;
+}
+
+if (localStorage.getItem('token')) {
+  token.value = localStorage.getItem('token');
+  has_verified.value = true;
+  if (localStorage.getItem('auto_login') == "true") {
+    overlay_visible.value = true;
+    test_token();
+  }
+}
 
 </script>
 
 <template>
+  <t-overlay :visible="overlay_visible" />
   <div v-if="!has_tested">
     <h1 style="text-align: center;">远程打卡小工具</h1>
     <t-divider />
@@ -147,8 +184,9 @@ const get_daka_config = async () => {
     <t-input v-model="token" placeholder="请输入www_token" class="home-input" @change="verify_input"
       :tips="errorMessage"></t-input>
     <div v-if="has_verified" style="text-align: center;">
-      <div style="text-align: center;margin: 20px auto 0;width: 70%;">
+      <div style="text-align: center;margin: 0 auto 0;width: 70%;">
         <t-button theme="primary" variant="light" @click="test_token" block>登录账号</t-button>
+
       </div>
       <div style="text-align: left;font-size: small; color: grey;margin-top: 10px;">1.登录成功后，Token将保存在浏览器本地，方便下次快速登录
       </div>
@@ -158,51 +196,29 @@ const get_daka_config = async () => {
   </div>
   <!-- token已提交测试 -->
   <div v-if="has_tested">
-    <p style="font-size: larger;font-weight: bold;text-align: center;margin: 20px auto 5px;">用户信息</p>
+    <!-- 用户信息卡片 -->
     <div class="card">
-      <p style="">用户名称：{{ account_info.nick_name }}</p>
-      <p style="">绑定手机：{{ account_info.phone }}</p>
-      <p style="">团队身份：{{ account_info.team_name }} - {{ account_info.name }}</p>
-      <p style="">打卡规则：{{ account_info.rule }}</p>
+      <div @click="toggleCard(1)">
+        <p style="font-size: larger;font-weight: bold;text-align: center;"
+          :style="{ color: isCollapsed1 ? '#c1c1c1' : '#333' }">用户信息</p>
+        <icon-font class="toggle-icon" :name="isCollapsed1 ? 'expand-down' : 'expand-up'"></icon-font>
+      </div>
+      <div class="card-content" style="margin: 0 auto 5px;" v-show="!isCollapsed1">
+        <p style="">用户名称：{{ account_info.nick_name }}</p>
+        <p style="">绑定手机：{{ account_info.phone }}</p>
+        <p style="">团队身份：{{ account_info.team_name }} - {{ account_info.name }}</p>
+        <p style="">打卡规则：{{ account_info.rule }}</p>
+      </div>
     </div>
     <br>
-    <div v-if="today_status">
-      <t-divider />
-      <p style="font-size: larger;font-weight: bold;text-align: center;margin: 20px auto 5px;">今日打卡信息</p>
-      <div class="card">
-        <p style="font-weight: bold;text-align: center;">当前班次打卡</p>
-        <div v-if="today_status.current">
-          <div>
-            <div v-for="(item, index) in today_status.current.details" :key="index">
-              <p>{{ item.desc }}：{{ item.statusDesc }}
-              <div v-if="item.currentTag" style="color: red;display: contents;">&nbsp;[当前打卡点]</div>
-              </p>
-            </div>
-          </div>
-        </div>
-        <p style="font-weight: bold;text-align: center;margin-top: 10px;">其他班次打卡</p>
-        <div v-if="today_status.others">
-          <div v-for="(shift, shiftIndex) in today_status.others" :key="shiftIndex" class="shift">
-            <div>
-              <div v-for="(item, index) in shift.details" :key="index">
-                <p>{{ item.desc }}：{{ item.statusDesc }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div style="width: 80%;text-align: center;margin: 0 auto;">
-          <t-button theme="light" variant="outline" @click="refresh_today_status" block>刷新列表</t-button>
-        </div>
-
+    <!-- 模拟位置信息卡片 -->
+    <div v-if="daka_config" class="card">
+      <div @click="toggleCard(2)">
+        <p style="font-size: larger; font-weight: bold; text-align: center;"
+          :style="{ color: isCollapsed2 ? '#c1c1c1' : '#333' }">位置模拟</p>
+        <icon-font class="toggle-icon" :name="isCollapsed2 ? 'expand-down' : 'expand-up'"></icon-font>
       </div>
-      <br>
-      <t-divider />
-      <div style="display: flex; align-items: center; justify-content: center;">
-        <p style="font-size: larger; font-weight: bold; text-align: center; flex: 1;">模拟定位信息</p>
-        <!-- <t-button theme="primary" variant="outline" @click="" size="smaller">修改定位信息</t-button> -->
-      </div>
-
-      <div v-if="daka_config" style="margin-left: 20px;">
+      <div class="card-content" style="margin-left: 20px;" v-show="!isCollapsed2">
         <p style="">省市街道：{{ daka_config.address }}</p>
         <p style="">打卡地址：{{ daka_config.location }}</p>
         <p style="">经度：{{ daka_config.longitude }} </p>
@@ -211,23 +227,63 @@ const get_daka_config = async () => {
         <p style="">Wi-Fi Mac地址：{{ daka_config.wifi_mac }} </p>
         <p style="">定位点随机偏移：50 米</p>
       </div>
+    </div>
+
+    <br>
+
+    <!-- 今日打卡信息卡片 -->
+    <div v-if="today_status">
+      <!-- <t-divider /> -->
+      <p style="font-size: larger;font-weight: bold;text-align: center;margin: 1% auto 5px;">今日打卡信息</p>
+      <div class="card">
+        <p style="font-weight: bold;text-align: center;">当前班次打卡</p>
+        <div v-if="today_status.current">
+          <div>
+            <div v-for="(item, index) in today_status.current.details" :key="index" style="margin-left: 20px;">
+              <p>{{ item.desc }}：{{ item.statusDesc }}
+              <div v-if="item.currentTag" style="color: red;display: contents;">&nbsp;[当前打卡点]</div>
+              </p>
+            </div>
+          </div>
+        </div>
+        <p style="font-weight: bold;text-align: center;margin-top: 10px;">其他班次打卡</p>
+        <div v-if="today_status.others">
+          <div v-for="(shift, shiftIndex) in today_status.others" :key="shiftIndex" class="shift"
+            style="margin-left: 20px;">
+            <div>
+              <div v-for="(item, index) in shift.details" :key="index">
+                <p>{{ item.desc }}：{{ item.statusDesc }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style="width: 50%;text-align: center;margin: 0 auto;">
+          <t-button theme="light" variant="outline" @click="refresh_today_status" block>刷新列表</t-button>
+        </div>
+      </div>
+      <br>
+
+
       <br>
       <div style="text-align: center;">
-        <t-button theme="primary" @click="daka" style="text-align: center;width: 70%;margin: 0 20px">一键打卡</t-button>
+        <t-button theme="primary" @click="daka"
+          style="font-size: 20px;letter-spacing: 3px;text-align: center;width: 70%;height: 60px;margin: 0 20px;box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">一键打卡</t-button>
       </div>
-      <div style="text-align: left;font-size: small; color: grey;margin-top: 10px;">1.请勿连续点击按钮，防止重复打卡</div>
-      <div style="text-align: left;font-size: small; color: grey;">2.本工具依赖于管理员开启“手机定位打卡”功能，若管理员关闭，则无法进行远程打卡</div>
-      <div style="text-align: left;font-size: small; color: grey;">3.本工具经过严格测试，其效果等同于在小程序中直接定位打卡</div>
-      <div style="text-align: left;font-size: small; color: grey;">4.本工具仅供学习交流，请合理使用</div>
+      <div style="text-align: center;font-size: small; color: grey;margin-top: 10px;">请勿连续点击按钮，防止重复打卡</div>
+      <div style="text-align: center;font-size: small; color: grey;">仅供学习交流，请合理使用</div>
     </div>
   </div>
   <t-divider />
+  <div
+    style="text-align: center;font-size: small; color: grey;display: flex;align-items: center;justify-content: center;margin-top: 30px;">
+    <div>下次自动登录&nbsp;</div>
+    <t-switch size="small" :default-value="true" :icon="icons" v-model="auto_login"
+      @change="save_auto_login"></t-switch>
+  </div>
   <!-- github开源地址 -->
-  <p style="text-align: center;margin-top: 20px;">
+  <p style="text-align: center;margin-top: 5px;">
     <a href="https://github.com/Little-King2022/HikIOT_signature" target="_blank">
       <img alt="GitHub" src="https://img.shields.io/badge/Github-%E9%A1%B9%E7%9B%AE%E5%9C%B0%E5%9D%80-blue">&nbsp;
-      <img alt="GitHub last commit"
-        src="https://img.shields.io/github/last-commit/Little-King2022/HikIOT_signature">&nbsp;
       <img alt="GitHub stars"
         src="https://img.shields.io/github/stars/Little-King2022/HikIOT_signature.svg?style=social">
     </a>
@@ -236,7 +292,7 @@ const get_daka_config = async () => {
 
 <style scoped>
 .home-input {
-  margin: 20px auto;
+  margin: 20px auto 10px;
   display: block;
   width: 90%;
   border: 1px solid rgba(220, 220, 220, 1);
@@ -255,5 +311,33 @@ const get_daka_config = async () => {
   border-radius: 8px;
   box-shadow: 6px 6px 8px rgba(0, 0, 0, 0.3);
   animation: fade-in 0.2s;
+  position: relative;
+  overflow: hidden;
+  transition: height 1s ease;
+}
+
+.card-content {
+  transition: max-height 0.3s ease-in-out;
+}
+
+.toggle-icon {
+  position: absolute;
+  top: 12px;
+  right: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  transition: transform 1s ease;
+}
+
+
+
+.card.collapsed .toggle-icon {
+  transform: rotate(180deg);
+  /* 旋转图标，表示折叠状态 */
+}
+
+/* switch开关 */
+t-switch {
+  height: 1px;
 }
 </style>
